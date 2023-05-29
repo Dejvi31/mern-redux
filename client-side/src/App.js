217f7dfa47c2e53fc1128db4f21323ec5f36e8b3
dejvi <dejviepadejvi@gmail.com>
2023-05-29 12:03:13 +0200

    shtuam bootstrap 4.6 dhe mbaruam Header

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import LandingPage from "./screens/LandingPage/LandingPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <LandingPage />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
