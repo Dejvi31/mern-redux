b86611f31a3ddc60270e4c728d967aa88cfdf59a
dejvi <dejviepadejvi@gmail.com>
2023-05-30 14:30:02 +0200

    lidhem mongodb me backendin dhe lidhem backun me frontin

--- client-side/src/screens/MyNotes/MyNotes.js
+++ client-side/src/screens/MyNotes/MyNotes.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
 import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import axios from "axios";
 
 const MyNotes = () => {
+  const [notes, setNotes] = useState([]);
+
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+    }
+  };
+  const fetchNotes = async () => {
+    const { data } = await axios.get("http://127.0.0.1:5000/api/notes");
+    console.log(data);
+    setNotes(data);
+  };
+  useEffect(() => {
+    fetchNotes();
+  }, []);
   return (
     <MainScreen title="Welcome back Dejvi">
       <Link to="createnote">
@@ -11,50 +26,59 @@ const MyNotes = () => {
           Create New Note
         </Button>
       </Link>
-      <Accordion defaultActiveKey={["0"]}>
-        <Accordion.Item eventkey="0">
-          <Card style={{ margin: 10 }}>
-            <Card.Header style={{ display: "flex" }}>
-              <span
-                style={{
-                  color: "black",
-                  textDecoration: "none",
-                  flex: 1,
-                  cursor: "pointer",
-                  alignSelf: "center",
-                  fontSize: 18,
-                }}
-              >
-                <Accordion.Button as={Card.Text} variant="link">
-                  Title
-                </Accordion.Button>
-              </span>
-              <div>
-                <Button>Edit</Button>
-                <Button variant="danger" className="mx-2">
-                  Delete
-                </Button>
-              </div>
-            </Card.Header>
-            <Accordion.Collapse>
-              <Card.Body>
-                <h4>
-                  <Badge bg="success" text="light">
-                    Category - Gym
-                  </Badge>
-                </h4>
 
-                <blockquote className="blockquote mb-0">
-                  <p>Content</p>
-                  <footer className="blockquote-footer">
-                    Created on - date
-                  </footer>
-                </blockquote>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion.Item>
-      </Accordion>
+      {notes.map((note) => {
+        return (
+          <Accordion defaultActiveKey={["0"]} key={note._id}>
+            <Accordion.Item eventkey="0">
+              <Card style={{ margin: 10 }}>
+                <Card.Header style={{ display: "flex" }}>
+                  <span
+                    style={{
+                      color: "black",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18,
+                    }}
+                  >
+                    <Accordion.Button as={Card.Text} variant="link">
+                      {note.title}
+                    </Accordion.Button>
+                  </span>
+                  <div>
+                    <Button href={`/note/${note._id}`}>Edit</Button>
+                    <Button
+                      variant="danger"
+                      className="mx-2"
+                      onClick={() => deleteHandler(note._id)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </Card.Header>
+                <Accordion.Collapse>
+                  <Card.Body>
+                    <h4>
+                      <Badge bg="success" text="light">
+                        Category - {note.category}
+                      </Badge>
+                    </h4>
+
+                    <blockquote className="blockquote mb-0">
+                      <p>{note.content}</p>
+                      <footer className="blockquote-footer">
+                        Created on - date
+                      </footer>
+                    </blockquote>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion.Item>
+          </Accordion>
+        );
+      })}
     </MainScreen>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const notes = require("./data/notes");
 const app = express();
+const cors = require("cors");
 dotenv.config();
+const connectDB = require("./config/db");
 
+app.use(cors());
+connectDB();
 app.get("/", (req, res) => {
   res.send("Hello NODE API");
 });
 
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started at port ${PORT}`));
