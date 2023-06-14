fd5173a64183c826ad4250cb920edade681c22b0
dejvi <dejviepadejvi@gmail.com>
2023-06-14 10:55:03 +0200

    changes

--- server/server.js
+++ server/server.js
@@ -14,9 +14,6 @@ const path = require("path");
 app.use(cors());
 connectDB();
 
-app.use("/api/users", userRoutes);
-app.use("/api/notes", noteRoutes);
-
 app.use(
   cors({
     origin: "https://note-epa.netlify.app/",
@@ -24,22 +21,24 @@ app.use(
 );
 // ------------- deployment -----------
 
-// __dirname = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client-side/build")));
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client-side/build")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(
-//       path.resolve(__dirname, "../client-side", "build", "index.html")
-//     );
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("Hello NODE API");
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../client-side", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello NODE API");
+  });
+}
 
 // -------------- deployment -------------
+app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
